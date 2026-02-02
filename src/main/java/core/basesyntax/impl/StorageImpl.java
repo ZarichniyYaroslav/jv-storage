16bc91d4c573ef96f387fbb4de5731b81b147520
jaryk <zarichniyyaroslav@gmail.com>
2026-02-02 15:26:34 +0100

    Added constant for array length, added new method for help searching an index , didnt use Objects.equals

--- src/main/java/core/basesyntax/impl/StorageImpl.java
+++ src/main/java/core/basesyntax/impl/StorageImpl.java
@@ -1,45 +1,48 @@
 package core.basesyntax.impl;
 
 import core.basesyntax.Storage;
-import java.util.Objects;
 
 public class StorageImpl<K, V> implements Storage<K, V> {
+    private static final int ARRAY_SIZE = 10;
     private int sizeValue;
     private Object[] keyArray;
     private Object[] valueArray;
 
     public StorageImpl() {
-        this.keyArray = new Object[10];
+        this.keyArray = new Object[ARRAY_SIZE];
         this.sizeValue = 0;
-        this.valueArray = new Object[10];
+        this.valueArray = new Object[ARRAY_SIZE];
     }
 
     @Override
     public void put(K key, V value) {
-        for (int i = 0; i < sizeValue; i++) {
-            if (Objects.equals(this.keyArray[i], key)) {
-                this.keyArray[i] = key;
-                this.valueArray[i] = value;
-                return;
-            }
+        if (indexHelper(key) == -1) {
+            this.keyArray[sizeValue] = key;
+            this.valueArray[sizeValue] = value;
+            sizeValue++;
         }
-        this.keyArray[sizeValue] = key;
-        this.valueArray[sizeValue] = value;
-        sizeValue++;
+        this.keyArray[indexHelper(key)] = key;
+        this.valueArray[indexHelper(key)] = value;
     }
 
     @Override
     public V get(K key) {
-        for (int i = 0; i < sizeValue; i++) {
-            if (Objects.equals(this.keyArray[i], key)) {
-                return (V) this.valueArray[i];
-            }
-        }
-        return null;
+        return indexHelper(key) == -1 ? null : (V) this.valueArray[indexHelper(key)];
     }
 
     @Override
     public int size() {
         return sizeValue;
     }
+
+    public int indexHelper(K key) {
+        for (int i = 0; i < sizeValue; i++) {
+            if ((this.keyArray[i] == key)
+                    || ((this.keyArray[i] != null) && this.keyArray[i].equals(key))) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
 }
